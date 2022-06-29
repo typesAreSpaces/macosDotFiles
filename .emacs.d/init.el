b4925b52b1f5e4011ca46640f7c239911118b918
Jose Abel Castellanos Joo <types@Joses-MBP.unm.edu>
2022-06-28 21:47:41 -0600

    Update config files.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -527,6 +527,13 @@
   :commands (org-capture org-agenda)
   :hook (org-mode . efs/org-mode-setup)
   :config
+  (setq org-file-apps '((auto-mode . emacs)
+                        (directory . emacs)
+                        ("\\.mm\\'" . default)
+                        ("\\.x?html?\\'" . default)
+                        ("\\.pdf\\'" . "zathura %s")))
+
+
   (setq org-ellipsis " ▾")
 
   (setq org-agenda-start-with-log-mode t)
